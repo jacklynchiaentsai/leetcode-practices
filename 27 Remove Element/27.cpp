e77a3abbeeef2a7aef360ca6e1d6e8301e4fbc11
Jacklyn (Chia-En) Tsai <101374527+jacklynchiaentsai@users.noreply.github.com>
2023-07-09 09:18:43 -0400

    Create 27.cpp

---
+++ 27 Remove Element/27.cpp
@@ -0,0 +1,9 @@
+// array content does not matter so can sort and then use eraase
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        sort(nums.begin(), nums.end());
+        nums.erase(lower_bound(nums.begin(), nums.end(), val), upper_bound(nums.begin(), nums.end(), val));
+        return nums.size();
+    }
+};
