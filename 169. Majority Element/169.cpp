7f010914428f6b8645091e0195b4698c4b81ca29
Jacklyn (Chia-En) Tsai <101374527+jacklynchiaentsai@users.noreply.github.com>
2023-07-09 10:43:11 -0400

    Create 169.cpp

---
+++ 169. Majority Element/169.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // solution in linear time and O(1) space complexity
+    int majorityElement(vector<int>& nums) {
+        int element = nums[0];
+        int count = 1;
+        for(int i =1; i<nums.size();i++){
+            if (count == 0){
+                element = nums[i];
+            }
+            
+            if (nums[i] == element){
+                count += 1;
+            } else{
+                count -= 1;
+            }
+
+            
+        }
+
+        return element;
+    }
+};
