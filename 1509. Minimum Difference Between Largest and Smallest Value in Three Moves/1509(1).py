6f56c1519660955014f49122495816d805a2c047
Jacklyn (Chia-En) Tsai <101374527+jacklynchiaentsai@users.noreply.github.com>
2024-10-28 23:02:20 -0400

    Create 1509(1).py

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves/1509(1).py
@@ -0,0 +1,42 @@
+"""
+greedy + math + efficient partial sort with heapq
+time: O(n)
+space: O(1)
+"""
+import heapq
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) <= 4:
+            return 0
+        
+        nums.sort()
+        four_largest = heapq.nlargest(4, nums)
+        four_largest.sort()
+        four_smallest = heapq.nsmallest(4, nums)
+
+        print(four_largest)
+        print(four_smallest)
+
+        mindiff = 1e18
+        for i in range(4):
+            mindiff = min(mindiff, four_largest[i] - four_smallest[i])
+
+        return mindiff
+
+"""
+intuition:
+changing the largest and smallest value also redefines the largest and smallest value
+
+len(nums) <= 4:
+return 0
+
+len(nums) > 4:
+given limitation of 3 steps evaluate the mindiff
+4thlargest, 3rdlargest, 2ndlargest, largest 
+smallest, 2ndsmallest, 3rdsmallest, 4thsmallest
+
+1,2,3,4,5
+2,3,4,5
+1,2,3,4
+"""
